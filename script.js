6f65c753ce69aa2471129830635b45f6fdca0061
pinto22 <51886210+Pinto2291@users.noreply.github.com>
2023-10-17 08:01:17 -0400

    nueva funcion de javascript agregada

--- script.js
+++ script.js
@@ -11,47 +11,67 @@ let main = document.getElementById('main');
 let button_fizzbuzz = document.getElementById('fizzbuzz');
 let button_reset = document.getElementById('reset');
 
+// Funciona generadora de FizzBuzz
+
 function fizzBuzzGenerator() {
 
     let i = 1;
     
     const interval = setInterval(() => {
-
+        let text = document.createElement('p');
+        text.classList.add('text-style');
     if (i % 15 === 0) {
         console.log(`El numero: ${i} fizzbuzz es multiplo de 3 y 5`);
-        let text = document.createElement('p');
-        text.classList.add('text-style');
         text.innerText = `fizzbuzz`;
-        main.appendChild(text);
     } else if (i % 3 === 0) {
         console.log(`El numero: ${i} fizz es multiplo de 3`);
-        let text = document.createElement('p');
-        text.classList.add('text-style');
         text.innerText = `fizz`;
-        main.appendChild(text);
     } else if (i % 5 === 0) {
         console.log(`El numero ${i} buzz es multiplo de 5`);
-        let text = document.createElement('p');
-        text.classList.add('text-style');
         text.innerText = `buzz`;
-        main.appendChild(text);
     } else {
         console.log(i);
-        let text = document.createElement('p');
-        text.classList.add('text-style');
         text.innerText = `${i}`;
-        main.appendChild(text);
     }
         i++;
+        main.appendChild(text);
+
     if (i > 100) {
         clearInterval(interval);
         }
     }, 50);
 }
 
+function newFizzbuzz() {
+
+        for(let i = 1; i <= 100; i++){
+
+            let text = document.createElement('p');
+            text.classList.add('text-style');
+
+            if(i % 3 == 0 && i % 5 == 0){
+                console.log('fizzbuzz nuevo');
+                text.innerText = `fizzbuzz`;
+            } else if(i % 3 == 0){
+                console.log('fizz');
+                text.innerText = `fizz`;
+            } else if(i % 5 == 0){
+                console.log('buzz');
+                text.innerText = `buzz`;
+            } else {
+                console.log(i);
+                text.innerText = `${i}`;
+            }
+            main.appendChild(text);
+        } 
+}
+
 button_fizzbuzz.addEventListener('click', () => {
-    fizzBuzzGenerator();
+    //fizzBuzzGenerator();
+    newFizzbuzz();
 });
+
+// Funcion para remover todos los childs
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
